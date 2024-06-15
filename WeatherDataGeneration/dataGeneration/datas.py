b1fb37fb88e5f752a5345190f58265c0f120f875
amirooo amiran <amiran.amirhossein@gmail.com>
2024-06-15 15:58:23 +0330

    ♻️ 02_config datas generator

--- WeatherDataGeneration/dataGeneration/datas.py
+++ WeatherDataGeneration/dataGeneration/datas.py
@@ -10,25 +10,30 @@ from humidity import Humidity
 from windSpeed import WindSpeed
 
 # generat data
-rand_sensor = Sensor.sensor()
 
-rand_city, rand_place = Location.locations()
-rand_location = f'{rand_city} {rand_place}'
 
-rand_temp = Temperature.temp()
-time_stamp = datetime.datetime.now()
-_time = time_stamp.strftime('%Y-%m-%d %H:%M:%S')
-rand_humidity = Humidity.humidity()
-rand_wind_speed = WindSpeed.windspeed()
+locations = Location.locations()
 
-data = {
-    "sensor id" : rand_sensor,
-    "location" : rand_location,
-    "timestamp" : _time,
-    "temperature" : rand_temp,
-    "humidity" : rand_humidity,
-    "wind speed" : rand_wind_speed
-}
+while True:
+    for location in locations:
+        rand_sensor = Sensor.sensor()
+        loc = location
+        rand_temp = Temperature.temp()
+        time_stamp = datetime.datetime.now()
+        _time = time_stamp.strftime('%Y-%m-%d %H:%M:%S')
+        rand_humidity = Humidity.humidity()
+        rand_wind_speed = WindSpeed.windspeed()
 
-json_data = json.dumps(data, indent=5)
-print(json_data)
+        data = {
+            "sensor id" : rand_sensor,
+            "location" : location,
+            "timestamp" : _time,
+            "temperature" : rand_temp,
+            "humidity" : rand_humidity,
+            "wind speed" : rand_wind_speed
+        }
+
+        json_data = json.dumps(data, indent=5)
+        print(json_data)
+    print('##############################################')
+    time.sleep(3)
