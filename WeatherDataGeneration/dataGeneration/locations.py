361cb42c0799b2641ef581edd76ba4aa5fcb177c
amirooo amiran <amiran.amirhossein@gmail.com>
2024-06-15 15:56:37 +0330

    ♻️ 03_config location generator

--- WeatherDataGeneration/dataGeneration/locations.py
+++ WeatherDataGeneration/dataGeneration/locations.py
@@ -8,7 +8,10 @@ class Location:
         places = ["city park", "downtown", "suburb", "Riverside", "airport"]
         citys = ["Tehran", "Abadan", "Tabriz", "Mashhad", "Isfahan", "Shiraz"]
         # Generate random location data
-        rand_city = random.choice(citys)
-        rand_place = random.choice(places)
+        finaly_location = []
 
-        return rand_city, rand_place
+        for city in citys:
+            rand_place = random.choice(places)
+            rand_location = f'{city} {rand_place}'
+            finaly_location.append(rand_location)    
+        return finaly_location
