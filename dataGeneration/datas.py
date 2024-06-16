146f13a4960e4009580a350bbd6fc08f0c416737
amirooo amiran <amiran.amirhossein@gmail.com>
2024-06-16 11:27:03 +0330

    🗑️ reamove django base of the project

---
+++ dataGeneration/datas.py
@@ -0,0 +1,51 @@
+import datetime
+import time
+import json
+import random
+
+# Import the necessary modules
+from .locations import Location
+from .temperature import Temperature
+from .sensors import Sensor
+from .humidity import Humidity
+from .windSpeed import WindSpeed
+
+locations = Location.locations()
+
+base_temperatures = {location: random.randint(35, 65) for location in locations} 
+
+temps = {location: [] for location in locations}
+
+class data_set:
+    @staticmethod
+    def datas():
+        while True:
+            for location in locations:
+                rand_sensor = Sensor.sensor()
+                
+                base_temp = base_temperatures[location]
+                
+                rand_temp = Temperature.temp(base_temp)
+                r_temp = round(rand_temp, 1)
+
+                temps[location].append(r_temp)
+                
+                time_stamp = datetime.datetime.now()
+                _time = time_stamp.strftime('%Y-%m-%d %H:%M:%S')
+                rand_humidity = Humidity.humidity()
+                rand_wind_speed = WindSpeed.windspeed()
+
+                data = {
+                    "sensor id": rand_sensor,
+                    "location": location,
+                    "timestamp": _time,
+                    "temperature": r_temp,
+                    "humidity": rand_humidity,
+                    "wind speed": rand_wind_speed,
+                    "temperature recorded": temps[location]
+                }
+
+                json_data = json.dumps(data, indent=5)
+                print(json_data)
+                return json_data
+            time.sleep(10)
