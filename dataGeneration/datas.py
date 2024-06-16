c90f83d1115f068bb28eb2627a2fd2af2d64f815
amirooo amiran <amiran.amirhossein@gmail.com>
2024-06-16 12:41:59 +0330

    ♻️ config rabbitmq to publish datas

--- dataGeneration/datas.py
+++ dataGeneration/datas.py
@@ -2,50 +2,70 @@ import datetime
 import time
 import json
 import random
+import pika
 
 # Import the necessary modules
-from .locations import Location
-from .temperature import Temperature
-from .sensors import Sensor
-from .humidity import Humidity
-from .windSpeed import WindSpeed
+from locations import Location
+from temperature import Temperature
+from sensors import Sensor
+from humidity import Humidity
+from windSpeed import WindSpeed
 
 locations = Location.locations()
 
-base_temperatures = {location: random.randint(35, 65) for location in locations} 
-
+base_temperatures = {location: random.randint(35, 65) for location in locations}
 temps = {location: [] for location in locations}
 
-class data_set:
+class DataSet:
     @staticmethod
     def datas():
-        while True:
-            for location in locations:
-                rand_sensor = Sensor.sensor()
-                
-                base_temp = base_temperatures[location]
-                
-                rand_temp = Temperature.temp(base_temp)
-                r_temp = round(rand_temp, 1)
+        for x in range(1, 100):
+            try:
+                connection = pika.BlockingConnection(pika.ConnectionParameters('rabbitmq'))
+                channel = connection.channel()
+                channel.queue_declare(queue='sensor_data')
+            except Exception as e:
+                print(e)
+                time.sleep(1)
+            else:
+                break
 
-                temps[location].append(r_temp)
-                
-                time_stamp = datetime.datetime.now()
-                _time = time_stamp.strftime('%Y-%m-%d %H:%M:%S')
-                rand_humidity = Humidity.humidity()
-                rand_wind_speed = WindSpeed.windspeed()
+        try:
+            while True:
+                for location in locations:
+                    rand_sensor = Sensor.sensor()
+                    base_temp = base_temperatures[location]
+                    rand_temp = Temperature.temp(base_temp)
+                    r_temp = round(rand_temp, 1)
 
-                data = {
-                    "sensor id": rand_sensor,
-                    "location": location,
-                    "timestamp": _time,
-                    "temperature": r_temp,
-                    "humidity": rand_humidity,
-                    "wind speed": rand_wind_speed,
-                    "temperature recorded": temps[location]
-                }
+                    temps[location].append(r_temp)
+                    time_stamp = datetime.datetime.now()
+                    _time = time_stamp.strftime('%Y-%m-%d %H:%M:%S')
+                    rand_humidity = Humidity.humidity()
+                    rand_wind_speed = WindSpeed.windspeed()
 
-                json_data = json.dumps(data, indent=5)
-                print(json_data)
-                return json_data
-            time.sleep(10)
+                    data = {
+                        "sensor id": rand_sensor,
+                        "location": location,
+                        "timestamp": _time,
+                        "temperature": r_temp,
+                        "humidity": rand_humidity,
+                        "wind speed": rand_wind_speed,
+                        "temperature recorded": temps[location]
+                    }
+
+                    json_data = json.dumps(data, indent=5)
+                    print(json_data)
+                    
+                    channel.basic_publish(exchange='',
+                                          routing_key='sensor_data',
+                                          body=json_data)
+
+                time.sleep(10)
+        except KeyboardInterrupt:
+            print("Interrupted")
+        finally:
+            connection.close()
+
+if __name__ == "__main__":
+    DataSet.datas()
