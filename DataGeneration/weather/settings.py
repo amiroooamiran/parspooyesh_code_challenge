a8096d7524b3e9727f6ce0edbf798458106e7bb5
amirooo amiran <amiran.amirhossein@gmail.com>
2024-06-16 08:35:05 +0330

    🚛 move fiels for micro services

--- DataGeneration/weather/settings.py
+++ DataGeneration/weather/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'rest_framework',
+
+    # apps
+    'WeatherDataGeneration'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +80,12 @@ WSGI_APPLICATION = 'weather.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'WeatherDataGeneration',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': 'db', 
+        'PORT': '5432',
     }
 }
 
