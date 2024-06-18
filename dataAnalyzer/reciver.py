04607d2b35a82e614c9d484ed3b7e51cb842ad65
amirooo amiran <amiran.amirhossein@gmail.com>
2024-06-18 13:20:06 +0330

    📎 06_create analyzor on reciver

--- dataAnalyzer/reciver.py
+++ dataAnalyzer/reciver.py
@@ -2,9 +2,16 @@ import pika
 import json
 import time
 
+# Import the necessary modules
+from tempAnalyzer import filter_temp_check
+
 def callback(ch, method, properties, body):
     data = json.loads(body)
-    print(f"Received data:\n{json.dumps(data, indent=5)}")
+    print(data)
+    # Apply the filter
+    filter_temp_check(data)
+
+    ch.basic_ack(delivery_tag=method.delivery_tag)
 
 def main():
     connection = None
@@ -14,15 +21,13 @@ def main():
             channel = connection.channel()
             channel.queue_declare(queue='sensor_data')
 
-            channel.basic_consume(queue='sensor_data', on_message_callback=callback, auto_ack=True)
+            channel.basic_consume(queue='sensor_data', on_message_callback=callback, auto_ack=False)
+            # channel.basic_qos(prefetch_count=1)
 
             print('Waiting for messages. To exit press CTRL+C')
             channel.start_consuming()
         except Exception as e:
             print(f"Error: {e}")
-        finally:
-            if connection and connection.is_open:
-                connection.close()
-
+            time.sleep(1)
 if __name__ == "__main__":
     main()
