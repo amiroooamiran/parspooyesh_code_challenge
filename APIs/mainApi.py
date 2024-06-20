4d83a577a023f43405308a2dce65353f4cde67b9
amirooo amiran <amiran.amirhossein@gmail.com>
2024-06-20 22:05:17 +0330

    📎 01_create base of the mainAPI to get all datas

---
+++ APIs/mainApi.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI, HTTPException
+from fastapi.encoders import jsonable_encoder
+from dataAnalyzer.dataStorage.storage import MongoDBStorage
+
+app = FastAPI()
+
+# Initialize MongoDBStorage instance
+storage = MongoDBStorage()
+
+# Endpoint to fetch all data
+@app.get("/datas/")
+async def get_all_data():
+    try:
+        cursor = storage.collection.find({})
+        data = [doc for doc in cursor]
+
+        # Exclude MongoDB ObjectId from serialization
+        for item in data:
+            item['_id'] = str(item['_id'])  # Convert ObjectId to string
+
+        return data
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
+    finally:
+        storage.close()
