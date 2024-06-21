cb5a28081b683b5aadc76d76f7ff6e05754772b8
amirooo amiran <amiran.amirhossein@gmail.com>
2024-06-21 12:29:47 +0330

    ♻️ 02_config to get realtime of the datas and published in API

--- APIs/mainApi.py
+++ APIs/mainApi.py
@@ -10,16 +10,16 @@ storage = MongoDBStorage()
 # Endpoint to fetch all data
 @app.get("/datas/")
 async def get_all_data():
-    try:
-        cursor = storage.collection.find({})
-        data = [doc for doc in cursor]
+    while True:
+        try:
+            cursor = storage.collection.find({})
+            data = [doc for doc in cursor]
 
-        # Exclude MongoDB ObjectId from serialization
-        for item in data:
-            item['_id'] = str(item['_id'])  # Convert ObjectId to string
+            # Exclude MongoDB ObjectId from serialization
+            for item in data:
+                item['_id'] = str(item['_id'])  # Convert ObjectId to string
 
-        return data
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
-    finally:
-        storage.close()
+            return data
+        except Exception as e:
+            raise HTTPException(status_code=500, detail=f"Internal Server Error: {str(e)}")
+
